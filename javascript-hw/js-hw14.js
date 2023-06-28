120dbaecc936e0a2d050271ac8b072d59fc2bb95
met253 <matvii.savula@gmail.com>
2023-06-28 08:21:23 +0200

    dfgdfg

--- javascript-hw/js-hw14.js
+++ javascript-hw/js-hw14.js
@@ -68,3 +68,18 @@ const aptDescr = apartment.descr;
 const aptPrice = apartment.price;
 const aptTags = apartment.tags;
 
+//4
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+  name,
+  price,
+  image,
+  tags,
+};
+
+console.log(product.name);
