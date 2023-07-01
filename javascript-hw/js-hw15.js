ea6fe052729a1bffb40da7c898c821828445ba3d
met253 <matvii.savula@gmail.com>
2023-07-01 15:02:52 +0200

    322

--- javascript-hw/js-hw15.js
+++ javascript-hw/js-hw15.js
@@ -1,20 +1,56 @@
-const user = [
-{
-     mood: "happy";,
-},
+const user = {
 
-{
-     hobby: "happy";
-},
+}
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
 
-{
-     premium: false;
-},
-
-]
+console.log(user)
 
 const userKeys = Object.keys(user);
 
 for (const key of userKeys) {
-    console.log(`${key}: ${user[key]}`);
-  }
+     console.log(`${key}: ${user[key]}`);
+}
+
+//2
+const friend = {
+     name: "Andrew",
+     age: 15,
+     hobbie: "Computer games",
+
+     countProps() {
+          const friendKeys = Object.keys(this);
+          const quantity = friendKeys.length;
+          return quantity
+     }
+}
+
+console.log(friend.countProps());
+//3
+
+const employeesHome = {
+     employees: [
+          {
+               name: "Sergio",
+               rating: 9,
+          },
+          {
+               name: "Dmitry",
+               rating: 8,
+          },
+          {
+               name: "Petro",
+               rating: 10,
+          },
+     ],
+
+     findBestEmployee(employees){
+          const persons = this.employees
+         for (person of persons) {
+          if (person.rating) {
+              
+          }
+         } 
+     }
+}
