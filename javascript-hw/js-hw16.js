20b20bb65acfa165555ce733daf918648ccaf7df
met253 <matvii.savula@gmail.com>
2023-07-14 08:58:48 +0200

    asdf

---
+++ javascript-hw/js-hw16.js
@@ -0,0 +1,13 @@
+const Account = {
+    accountName: samiksmartik,
+    balance: 42.069,
+    var button = document.getElementById("withdraw");
+
+
+button.addEventListener("click", function() {
+ 
+  alert("Withdrawed (amount) of money!");
+});
+}
+
+
