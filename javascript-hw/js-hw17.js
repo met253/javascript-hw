8b8bba5de28a62bdaf332c082a8968e3b4e608bd
met253 <matvii.savula@gmail.com>
2023-07-15 09:06:45 +0200

    5

---
+++ javascript-hw/js-hw17.js
@@ -0,0 +1,63 @@
+const musicLibrary = {
+    count: 2,
+    artists: [
+        {
+            name: "The Beatles",
+            albums: [
+                {
+                    title: "Sgt. Pepper\'s Lonely Heards Club Band",
+                    year: 1967,
+                    tracks: [
+                        {
+                            title: "With a Little Help from My Friends",
+                            duration: "2:44"
+                        },
+                        {
+                            title: "Lucy in the Sky with Diamonds",
+                            duration: "3:28"
+                        },
+                        {
+                            title: "A Day in the Life",
+                            duration: "5:33"
+                        }
+                    ]
+                },
+                {
+                    title: "Abbey Road",
+                    year: 1969,
+                    tracks: [
+                        { title: "Come Together", duration: "4:19" },
+                        { title: "Something", duration: "3:01" },
+                        { title: "Here Comes the Sun", duration: "3:06" },
+
+                    ]
+
+                }
+            ]
+        },
+        {
+            name: "Pink Floyd",
+            albums: [
+                {
+                    title: "The Wall",
+                    year: 1979,
+                    tracks: [
+                        { title: "Antoher Brick in the Wall, Part 2", duration: "3:59" },
+                        { title: "Comfortably Numb", duration: "6:23"},
+                        { title: "Hey You", duration: "4:40"}
+                    ]
+                },
+                {
+                    title: "Dark Side of the Moon",
+                    year: 1973,
+                    tracks: [
+                        {title: "Speak to Me/Breathe", duration: "3:58"},
+                        {title: "Time", duration: "7:06"},
+                        {title: "Money", duration: "6:22"}
+                    ]
+                }
+            ]
+        }
+
+    ]
+}
