37297e5ba6c52bc91f97f994af6305dfbe151ec6
met253 <matvii.savula@gmail.com>
2023-07-17 08:57:04 +0200

    t

--- javascript-hw/js-hw17.js
+++ javascript-hw/js-hw17.js
@@ -2,33 +2,33 @@ const musicLibrary = {
     count: 2,
     artists: [
         {
-            name: "The Beatles",
-            albums: [
+            name1: "The Beatles",
+            albums1: [
                 {
-                    title: "Sgt. Pepper\'s Lonely Heards Club Band",
-                    year: 1967,
-                    tracks: [
+                    albumTitle1: "Sgt. Pepper\'s Lonely Heards Club Band",
+                    albumYear1: 1967,
+                    tracks1: [
                         {
-                            title: "With a Little Help from My Friends",
-                            duration: "2:44"
+                            title1: "With a Little Help from My Friends",
+                            duration1: "2:44"
                         },
                         {
-                            title: "Lucy in the Sky with Diamonds",
-                            duration: "3:28"
+                            title2: "Lucy in the Sky with Diamonds",
+                            duration2: "3:28"
                         },
                         {
-                            title: "A Day in the Life",
-                            duration: "5:33"
+                            title3: "A Day in the Life",
+                            duration3: "5:33"
                         }
                     ]
                 },
                 {
-                    title: "Abbey Road",
-                    year: 1969,
-                    tracks: [
-                        { title: "Come Together", duration: "4:19" },
-                        { title: "Something", duration: "3:01" },
-                        { title: "Here Comes the Sun", duration: "3:06" },
+                    albumTitle2: "Abbey Road",
+                    albumYear2: 1969,
+                    album2: [
+                        { title4: "Come Together", duration7: "4:19" },
+                        { title5: "Something", duration8: "3:01" },
+                        { title6: "Here Comes the Sun", duration9: "3:06" },
 
                     ]
 
@@ -36,24 +36,24 @@ const musicLibrary = {
             ]
         },
         {
-            name: "Pink Floyd",
-            albums: [
+            name2: "Pink Floyd",
+            albums2: [
                 {
-                    title: "The Wall",
-                    year: 1979,
-                    tracks: [
-                        { title: "Antoher Brick in the Wall, Part 2", duration: "3:59" },
-                        { title: "Comfortably Numb", duration: "6:23"},
-                        { title: "Hey You", duration: "4:40"}
+                    albumTitle3: "The Wall",
+                    albumYear3: 1979,
+                    tracks3: [
+                        { title10: "Antoher Brick in the Wall, Part 2", duration10: "3:59" },
+                        { title11: "Comfortably Numb", duration11: "6:23" },
+                        { title12: "Hey You", duration12: "4:40" }
                     ]
                 },
                 {
                     title: "Dark Side of the Moon",
                     year: 1973,
                     tracks: [
-                        {title: "Speak to Me/Breathe", duration: "3:58"},
-                        {title: "Time", duration: "7:06"},
-                        {title: "Money", duration: "6:22"}
+                        { title13: "Speak to Me/Breathe", duration13: "3:58" },
+                        { title14: "Time", duration14: "7:06" },
+                        { title15: "Money", duration15: "6:22" }
                     ]
                 }
             ]
