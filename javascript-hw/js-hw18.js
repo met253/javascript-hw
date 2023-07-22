72c5ecf4d28edeb15b48649bc99c806788b0dec5
met253 <matvii.savula@gmail.com>
2023-07-22 08:51:01 +0200

    asdf

--- javascript-hw/js-hw18.js
+++ javascript-hw/js-hw18.js
@@ -1,1 +1,58 @@
-const name = [];
+//1
+const names = (namess) => {
+  const find = coll.map((obj) => obj.name);
+  console.log(find);
+};
+
+const namesFound = [{ name: "sus" }, { name: "kvas" }, { name: "owo" }];
+
+names(namesFound);
+//2
+const eyeColors = (eyee) => {
+    const findEye = coll.map((obj) => obj.eye);
+    console.log(findEye);
+  };
+  
+  const colorsFound = [{ name: "blue" }, { name: "green" }, { name: "brown" }];
+  
+  names(colorsFound);
+  //3
+  const findGender = (coll, gender) => {
+    const someGender = coll.filter((person) => person.gender === gender);
+    console.log(someGender);
+    const genderOfNames = someGender.map((person) => person.name);
+    return genderOfNames;
+  };
+  
+  const nameGenders = [
+    {
+      name: "waowo",
+      gender: "male",
+    },
+    {
+      name: "amongus",
+      gender: "male_07",
+    },
+    {
+      name: "John",
+      gender: "female",
+    },
+  ];
+  
+  findSomePeople(nameGenders, "male");
+  //5
+  const users = [
+    { name: "aa", email: "aa@gmail.com", age: 1 },
+    { name: "bb", email: "bb@gmail.com", age: 2 },
+    { name: "cc", email: "cc@gmail.com", age: 3 },
+    { name: "dd", email: "dd@gmail.com", age: 3 },
+  ];
+  
+  const findEmail = (array, email) => {
+    const foundEmail = array.find((person) => person.email === email);
+    console.log(foundEmail);
+    return foundEmail;
+  };
+  
+  findEmail(users, "aa@gmail.com");
+  
