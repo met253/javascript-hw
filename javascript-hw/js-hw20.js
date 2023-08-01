2733feb0bfeb682df11a88ca5359896946840cf2
met253 <matvii.savula@gmail.com>
2023-08-01 09:02:46 +0200

    d

--- javascript-hw/js-hw20.js
+++ javascript-hw/js-hw20.js
@@ -1,26 +1,49 @@
-const users = [
-    { name: 'John', balance: 69 },
-    { name: 'Jane', balance: 1 },
-    { name: 'Bob', balance: 41999 },
-  ];
-  const totalBalance = users.reduce((acc, user) => acc + user.balance, 0);
-  console.log(totalBalance);
-  //2
-  const friends = [
-    {
-      name: "a",
-      friends: ["b", "c", "d"],
-    },
-    {
-      name: "b",
-      friends: ["a", "c", "d"],
-    },
-    {
-      name: "c",
-      friends: ["a", "b", "d"],
-    },
-    {
-      name: "d",
-      friends: ["a", "b", "c"],
-    },
-  ];
+// const users = [
+//     { name: 'John', balance: 69 },
+//     { name: 'Jane', balance: 1 },
+//     { name: 'Bob', balance: 41999 },
+//   ];
+//   const totalBalance = users.reduce((acc, user) => acc + user.balance, 0);
+//   console.log(totalBalance);
+//   //2
+//   const friends = [
+//     {
+//       name: "a",
+//       friends: ["b", "c", "d"],
+//     },
+//     {
+//       name: "b",
+//       friends: ["a", "c", "d"],
+//     },
+//     {
+//       name: "c",
+//       friends: ["a", "b", "d"],
+//     },
+//     {
+//       name: "d",
+//       friends: ["a", "b", "c"],
+//     },
+//   ];
+  //4
+  // const knowladge = [
+  //   { name: 'bas', skills: ['JavaScript', 'HTML', 'CSS'] },
+  //   { name: 'las', skills: ['Python', 'Java', 'SASS'] },
+  //   { name: 'gas', skills: ['JavaScript', 'Java', 'CSS'] },
+  //   { name: 'mаs', skills: ['Python', 'JavaScript', 'CSS'] },
+  // ];
+  
+
+  // function getAllUniqueSortedSkills(knowladge) {
+  //   const allSkills = knowladge.reduce((acc, user) => {
+  //     user.skills.forEach(skill => {
+  //       if (!acc.includes(skill)) {
+  //         acc.push(skill);
+  //       }
+  //     });
+  //     return acc;
+  //   }, []);
+  
+  //   return allSkills.sort();
+  // }
+  // const allUniqueSortedSkills = getAllUniqueSortedSkills(knowladge);
+  // console.log(allUniqueSortedSkills);
