e553fea1b7ec9e4aad5a046e0731bb4c5d1afaff
met253 <matvii.savula@gmail.com>
2023-08-15 09:00:30 +0200

    as

---
+++ javascript-hw/js-hw21.js
@@ -0,0 +1,15 @@
+//1
+const word = ("World");
+console.log(_.size(word));
+//2
+const cloned = _.clone(word);
+console.log(cloned);
+console.log(cloned === word);
+//3
+console.log(_.isNumber(word));
+//4
+console.log(_.words("hello@#&^!@(*#&) , @#bye@! , $%sus%^, @*^^&*%#"));
+//5
+const date = new Date();
+console.log(date);
+//6
