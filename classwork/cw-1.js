45fa3fee522b18f50bea41bb316acb4f61bf70e6
met253 <matvii.savula@gmail.com>
2023-08-17 18:39:24 +0200

    f

--- classwork/cw-1.js
+++ classwork/cw-1.js
@@ -1,6 +1,10 @@
-function countSum(basket) {
-let totalSum = 0;
-for (const products of basket) {
-    const {name, price, queantity}
-}
-}
+// function countSum(basket) {
+// let totalSum = 0;
+// for (const products of basket) {
+//     const {name, price, queantity}
+// }
+// }.
+console.dir(Object); 
+console.dir(Array)
+console.dir(String)
+console.dir(Number)
