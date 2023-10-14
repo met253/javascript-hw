3d10e802dc088451c67bc72a317e8ee78e53cdfe
met253 <matvii.savula@gmail.com>
2023-10-14 14:14:16 +0200

    task-3-hw-dom2.js

--- javascript-hw/homework-dom2.js
+++ javascript-hw/homework-dom2.js
@@ -15,18 +15,56 @@
 
 /////task 2
 
-const ingredientsList = document.getElementById("ingredients")
-const ingredients = [
-    'Картопля',
-    'Гриби',
-    'Часник',
-    'Помідори',
-    'Зелень',
-    'Приправи',
-  ];
+// const ingredientsList = document.getElementById("ingredients")
+// const ingredients = [
+//     'Картопля',
+//     'Гриби',
+//     'Часник',
+//     'Помідори',
+//     'Зелень',
+//     'Приправи',
+//   ];
 
-  const updateList = ingredients.map((ingredient) => {
-    const item = document.createElement("li")
-    item.textContent = ingredient
-    ingredientsList.append(item)
-  });
+//   const updateList = ingredients.map((ingredient) => {
+//     const item = document.createElement("li")
+//     item.textContent = ingredient
+//     ingredientsList.append(item)
+//   });
+  
+  /////////3
+
+
+
+//   const list = document.getElementById("gallery");
+// console.log(list)
+
+
+
+// const images = [
+//   {
+//     url:
+//       'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'White and Black Long Fur Cat',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+//   },
+//   {
+//     url:
+//       'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+//     alt: 'Group of Horses Running',
+//   },
+// ];
+
+// const galleryList = images.map((image) => {
+//   return ` <li>
+//  <img src="${image.url}" alt="${image.alt}">
+// </li> `
+
+// });
+// const newGalleryList = galleryList.join("");
+// list.insertAdjacentHTML("beforeend", newGalleryList);
+// console.dir(list);
+// console.log(newGalleryList);
