d8087c70bb499dd6535fbcdce07ebcc477780621
met253 <matvii.savula@gmail.com>
2023-10-16 19:02:42 +0200

    hello

---
+++ javascript-hw/actions.js
@@ -0,0 +1,18 @@
+const button = document.querySelector(button)
+const showModal = document.querySelector(show-modal)
+const buttonShowModal = document.querySelector()
+const showModalButton = document.qu     
+
+
+
+
+
+
+
+
+
+                
+        
+
+
+
