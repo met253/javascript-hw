7e6e1bad137cf15554c9cdd2552b17c683aa4ff9
met253 <matvii.savula@gmail.com>
2023-10-21 14:22:31 +0200

    actions1

--- javascript-hw/actions.js
+++ javascript-hw/actions.js
@@ -1,9 +1,21 @@
-const button = document.querySelector(button)
-const showModal = document.querySelector(show-modal)
-  
+const openBtn = document.querySelector("[data-action=open-modal]");
+const backdrop = document.querySelector(".backdrop");
+const closeBtn = document.querySelector("[data-action=close-modal]");
 
+function toggleFunction () {
+backdrop.classList.toggle("show-modal")
+};
 
-
+console.log(openBtn)
+console.log(closeBtn)
+openBtn.addEventListener("click", toggleFunction);
+closeBtn.addEventListener("click", toggleFunction);
+backdrop.addEventListener("click", (event) => {
+console.log(event.target)
+if (event.target.classList.contains("backdrop")) {
+    toggleFunction()
+}
+});
 
 
 
